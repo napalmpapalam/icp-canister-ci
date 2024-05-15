da790f39b3427e1b5df22a15cf5a5ca1cb22421f
Semen Loktionov <loktionov.kh@gmail.com>
2024-05-15 16:09:33 +0300

    try to update canister via ci[2]

--- scripts/build.sh
+++ scripts/build.sh
@@ -6,3 +6,4 @@ cargo build -p canister --release --target wasm32-unknown-unknown
 
 # Gzip wasm
 gzip -c target/wasm32-unknown-unknown/release/canister.wasm > wasm/canister.wasm.gz
+# wasm/canister_bf0eb831775b4a50a4f1526f6ee52007045d5b4e.wasm.gz
